949570f10d0be8c7a679e70c84f33a5d66b9ebd0
Siluat <siluat@gmail.com>
2018-07-25 23:29:08 +0900

    포지션 평가 주소 정보 및 주소 업데이트 코드

--- README.md
+++ README.md
@@ -13,4 +13,12 @@
 | Command | Description |
 | --- | --- |
 | node ./crawling/officialProfile.js | 공식 홈페이지에서 소녀들의 프로필 정보를 겟, DynamoDB에 저장 |
+| node ./crawling/positionDirectCam.js | 포지션 평가 직캠 관련 정보 수집, DynamoDB에 저장 |
 | node ./util/createBaseMap.js | 빈 참조 맵파일 생성 |
+| node ./dynamoDB/updatePositionDirectCamUrl.js | 포지션 평가 영상 주소 업데이트 |
+
+## Step
+
+1. node ./crawling/officialProfile.js
+2. node ./dynamoDB/updateData.js ./data/positionDirectCam.json
+3. node ./crawling/positionDirectCam.js
---
+++ dynamoDB/updatePositionDirectCamUrl.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient({
+  region: 'ap-northeast-2'
+});
+
+const DB_TABLE_NAME = 'produce48';
+const FILE_PATH = './data/positionDirectCam.json';
+
+console.info('포지션 평가 주소 업데이트 시작!');
+
+const items = JSON.parse(fs.readFileSync(FILE_PATH, 'utf8'));
+
+items.forEach(item => {
+  if (item.reference.length) {
+    store(item);
+  }
+});
+
+console.info('포지션 평가 주소 업데이트 완료!');
+
+function store(item) {
+  const params = {
+    TableName: DB_TABLE_NAME,
+    Key: {
+      id: item.id
+    },
+    UpdateExpression: 'set positionDirectCamUrl = :r',
+    ExpressionAttributeValues: {
+      ':r': item.reference
+    }
+  };
+
+  documentClient.update(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      console.error(params);
+      process.exit();
+    }
+  });
+};
