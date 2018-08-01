45b56b73fd567d5167bebede0aadf836c3e7b92e
Siluat <siluat@gmail.com>
2018-08-02 07:39:35 +0900

    국정원 크롤링에 단계 상승 시간 저장 기능 추가

--- crawling/gardensHug.js
+++ crawling/gardensHug.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const moment = require('moment');
 const AWS = require('aws-sdk');
 const documentClient = new AWS.DynamoDB.DocumentClient({region: 'ap-northeast-2'});
 const sns = new AWS.SNS({region: 'ap-northeast-2'});
@@ -39,11 +40,7 @@ const crawling = async items => {
 
     const hugRate = await page.evaluate(getHugRate);
 
-    store({
-      id: item.id,
-      name: item.name,
-      hugRate
-    });
+    store(item, hugRate);
   }
 
   await browser.close();
@@ -61,16 +58,92 @@ const getHugRate = () => {
   return parseFloat(document.querySelector(selector).style.width);
 };
 
-function store(item) {
+function store(item, hugRate) {
+  const updateExpression = ['set gardenHugRate = :ghr'];
+  let expresssionAttributeValues = { ':ghr' : hugRate };
+
+  if (hugRate === 6.6 && !item.gardenHugStep11Date) {
+    updateExpression.push('gardenHugStep11Date = :step11date');
+    expresssionAttributeValues[':step11date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 13.2 && !item.gardenHugStep12Date) {
+    updateExpression.push('gardenHugStep12Date = :step12date');
+    expresssionAttributeValues[':step12date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 20 && !item.gardenHugStep13Date) {
+    updateExpression.push('gardenHugStep13Date = :step13date');
+    expresssionAttributeValues[':step13date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 26.6 && !item.gardenHugStep21Date) {
+    updateExpression.push('gardenHugStep21Date = :step21date');
+    expresssionAttributeValues[':step21date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 33.2 && !item.gardenHugStep22Date) {
+    updateExpression.push('gardenHugStep22Date = :step22date');
+    expresssionAttributeValues[':step22date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 40 && !item.gardenHugStep23Date) {
+    updateExpression.push('gardenHugStep23Date = :step23date');
+    expresssionAttributeValues[':step23date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 46.6 && !item.gardenHugStep31Date) {
+    updateExpression.push('gardenHugStep31Date = :step31date');
+    expresssionAttributeValues[':step31date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 53.2 && !item.gardenHugStep32Date) {
+    updateExpression.push('gardenHugStep32Date = :step32date');
+    expresssionAttributeValues[':step32date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 60 && !item.gardenHugStep33Date) {
+    updateExpression.push('gardenHugStep33Date = :step33date');
+    expresssionAttributeValues[':step33date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 66.6 && !item.gardenHugStep41Date) {
+    updateExpression.push('gardenHugStep41Date = :step41date');
+    expresssionAttributeValues[':step41date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 73.2 && !item.gardenHugStep42Date) {
+    updateExpression.push('gardenHugStep42Date = :step42date');
+    expresssionAttributeValues[':step42date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 80 && !item.gardenHugStep43Date) {
+    updateExpression.push('gardenHugStep43Date = :step43date');
+    expresssionAttributeValues[':step43date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 86.6 && !item.gardenHugStep51Date) {
+    updateExpression.push('gardenHugStep51Date = :step51date');
+    expresssionAttributeValues[':step51date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 93.2 && !item.gardenHugStep52Date) {
+    updateExpression.push('gardenHugStep52Date = :step52date');
+    expresssionAttributeValues[':step52date'] = moment().format('YYYY-MM-DD');
+  }
+
+  if (hugRate === 100 && !item.gardenHugStep53Date) {
+    updateExpression.push('gardenHugStep53Date = :step53date');
+    expresssionAttributeValues[':step53date'] = moment().format('YYYY-MM-DD');
+  }
+
   const params = {
     TableName: DB_TABLE_NAME,
     Key: {
       id: item.id
     },
-    UpdateExpression: 'set gardenHugRate = :ghr',
-    ExpressionAttributeValues: {
-      ':ghr': item.hugRate
-    }
+    UpdateExpression: updateExpression.join(', '),
+    ExpressionAttributeValues: expresssionAttributeValues
   };
 
   console.info('Store ' + item.name + '\'s data...');
