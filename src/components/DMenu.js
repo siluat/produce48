89cf952d174e949b9c0f330f5ead4dcc9217d77b
Siluat <siluat@gmail.com>
2018-08-26 23:35:09 +0900

    Data3 메뉴 활성화

--- src/components/DMenu.js
+++ src/components/DMenu.js
@@ -10,14 +10,28 @@ const DMenu = () =>
       content='이 페이지의 콘텐츠는 모바일 환경을 정상적으로 지원하지 않습니다.'
     >
     </Message>
-    <Container style={{ paddingTop: '20px', paddingBottom: '20px' }}>
-      <Card as='a' href='/d1' target='_blank'>
+    <Container style={{ paddingTop: '20px', paddingBottom: '20px', textAlign: 'center' }}>
+      <Card as='a' href='/d1' target='_blank' 
+        style={{ display: 'inline-block' }}
+      >
         <Image src='/images/d/d1.png' />
         <Card.Content>
           <Card.Header>D1</Card.Header>
           <Card.Description>
             2차 순위 발표식<br/>
             TOP 30 한일 연습생 득표 점유 현황
+          </Card.Description>
+        </Card.Content>
+      </Card>
+      <Card as='a' href='/d3' target='_blank' 
+        style={{ display: 'inline-block' }}
+      >
+        <Image src='/images/d/d3.png' />
+        <Card.Content>
+          <Card.Header>D3</Card.Header>
+          <Card.Description>
+            연습생들 흐름도?<br/>
+            (<s>귀찮아서</s>미완성)
           </Card.Description>
         </Card.Content>
       </Card>
