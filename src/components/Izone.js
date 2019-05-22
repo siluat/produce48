eb2520ca6ccee4e98d168450126bd57deb001cfe
Siluat <siluat@gmail.com>
2019-05-22 22:54:42 +0900

    코드 정리

--- src/components/Izone.js
+++ src/components/Izone.js
@@ -1,75 +1,8 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Segment } from "semantic-ui-react";
-import { sortBy, maxBy } from "lodash";
-import FlipMove from "react-flip-move";
-import LoadingContent from "./LoadingContent";
-import Trainee from "./Trainee";
-import ProgressBar from "./ProgressBar";
-
-const PATH_FETCH = "data/produce48.json";
-
-const SORTS = {
-  VOTE: list => sortBy(list, "finalVote").reverse()
-};
-
-const filter = item => {
-  return !item.retired;
-};
 
 class Izone extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      traineeData: [],
-      maxVote: 0,
-      error: null,
-      isLoading: false
-    };
-
-    this.fetchTraineeData = this.fetchTraineeData.bind(this);
-    this.setTraineeData = this.setTraineeData.bind(this);
-  }
-
-  componentDidMount() {
-    this.fetchTraineeData();
-  }
-
-  componentDidUpdate() {
-    const progresses = document.querySelectorAll(
-      ".bar .progress, .outer-value"
-    );
-
-    for (let i = 0; i < progresses.length; i++) {
-      let t = progresses[i].textContent;
-      progresses[i].textContent = t
-        .toString()
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-  }
-
-  fetchTraineeData() {
-    this.setState({ isLoading: true });
-
-    axios(`${PATH_FETCH}`)
-      .then(result => this.setTraineeData(result.data.items))
-      .catch(error => this.setState({ error }));
-  }
-
-  setTraineeData(data) {
-    this.setState({
-      traineeData: data,
-      isLoading: false,
-      maxVote: maxBy(data, "finalVote").finalVote
-    });
-  }
-
   render() {
-    const { i18n, t } = this.props;
-
-    const { traineeData, maxVote, isLoading } = this.state;
-
     return (
       <div>
         <Segment
