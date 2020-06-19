103e71c2942610d895002dadd50e9eb0f320774b
Siluat <siluat@gmail.com>
2020-06-19 23:32:26 +0900

    파일 정리

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,29 @@
-import React, { Component } from "react";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import { Menu, Sticky, Dropdown, Label } from "semantic-ui-react";
-import ReactGA from "react-ga";
-import { I18n } from "react-i18next";
+import React, { Component } from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Menu, Sticky, Dropdown, Label } from 'semantic-ui-react';
+import ReactGA from 'react-ga';
+import { I18n } from 'react-i18next';
 
-import i18n from "./i18n";
+import i18n from './i18n';
 
-import ChangeLog from "./ChangeLog";
-import ConceptDirectCamRanking from "./ConceptDirectCamRanking";
-import ConceptEndingRanking from "./ConceptEndingRanking";
-import ConceptMusicRanking from "./ConceptMusicRanking";
-import D2 from "./D2";
-import Footer from "./Footer";
-import Garden from "./Garden";
-import GroupBattleDirectCamRanking from "./GroupBattleDirectCamRanking";
-import Izone from "./Izone";
-import MCountdownSpecialStageRanking from "./MCountdownSpecialStageRanking";
-import NekkoyaDirectCamRanking from "./NekkoyaDirectCamRanking";
-import NoMatch from "./NotMatch";
-import PositionDirectCamRanking from "./PositionDirectCamRanking";
-import PunchQueen from "./PunchQueen";
-import SpecialClipRanking from "./SpecialClipRanking";
+import ChangeLog from './ChangeLog';
+import ConceptDirectCamRanking from './ConceptDirectCamRanking';
+import ConceptEndingRanking from './ConceptEndingRanking';
+import ConceptMusicRanking from './ConceptMusicRanking';
+import D2 from './D2';
+import Footer from './Footer';
+import Garden from './Garden';
+import GroupBattleDirectCamRanking from './GroupBattleDirectCamRanking';
+import MCountdownSpecialStageRanking from './MCountdownSpecialStageRanking';
+import NekkoyaDirectCamRanking from './NekkoyaDirectCamRanking';
+import NoMatch from './NotMatch';
+import PositionDirectCamRanking from './PositionDirectCamRanking';
+import PunchQueen from './PunchQueen';
+import SpecialClipRanking from './SpecialClipRanking';
 
-import "./important.css";
-import VerticalCamRanking from "./VerticalCamRanking";
-import DMenu from "./DMenu";
+import './important.css';
+import VerticalCamRanking from './VerticalCamRanking';
+import DMenu from './DMenu';
 
 class App extends Component {
   constructor(props) {
@@ -34,8 +33,8 @@ class App extends Component {
       sidebar: false,
     };
 
-    if (process.env.NODE_ENV === "production") {
-      ReactGA.initialize("UA-122956473-2");
+    if (process.env.NODE_ENV === 'production') {
+      ReactGA.initialize('UA-122956473-2');
       ReactGA.pageview(window.location.pathname);
     }
   }
@@ -52,104 +51,104 @@ class App extends Component {
             <div ref={this.handleContextRef}>
               <Sticky context={contextRef} className="top-menu">
                 <Menu attached inverted>
-                  <Dropdown style={{ width: "68%" }} item text="Menu">
+                  <Dropdown style={{ width: '68%' }} item text="Menu">
                     <Dropdown.Menu>
                       <Dropdown.Item href="/garden">
-                        {t("top-menu-garden")}
+                        {t('top-menu-garden')}
                       </Dropdown.Item>
-                      {i18n.language !== "jp" && i18n.language !== "en" && (
+                      {i18n.language !== 'jp' && i18n.language !== 'en' && (
                         <Dropdown.Item href="/punchQueen">
-                          {t("top-menu-punchqueen")}
+                          {t('top-menu-punchqueen')}
                         </Dropdown.Item>
                       )}
                       <Dropdown.Item href="/mCountdown">
-                        {t("top-menu-m-countdown")}
+                        {t('top-menu-m-countdown')}
                       </Dropdown.Item>
                       <Dropdown.Item href="/concept">
-                        {t("top-menu-concept-trainee")}
+                        {t('top-menu-concept-trainee')}
                       </Dropdown.Item>
                       <Dropdown.Item href="/conceptMusic">
-                        {t("top-menu-concept-music")}
+                        {t('top-menu-concept-music')}
                       </Dropdown.Item>
                       <Dropdown.Item href="/conceptEnding">
-                        {t("top-menu-concept-ending")}
+                        {t('top-menu-concept-ending')}
                       </Dropdown.Item>
                       <Dropdown.Item href="/verticalCam">
-                        {t("top-menu-vertical-cam")}
+                        {t('top-menu-vertical-cam')}
                       </Dropdown.Item>
                       <Dropdown.Item href="/special">
-                        {t("top-menu-special-clip")}
+                        {t('top-menu-special-clip')}
                       </Dropdown.Item>
                       <Dropdown.Item href="/position">
-                        {t("top-menu-position")}
+                        {t('top-menu-position')}
                       </Dropdown.Item>
                       <Dropdown.Item href="/groupBattle">
-                        {t("top-menu-group")}
+                        {t('top-menu-group')}
                       </Dropdown.Item>
                       <Dropdown.Item href="/nekkoya">
-                        {t("top-menu-nekkoya")}
+                        {t('top-menu-nekkoya')}
                       </Dropdown.Item>
-                      {i18n.language !== "jp" && i18n.language !== "en" ? (
+                      {i18n.language !== 'jp' && i18n.language !== 'en' ? (
                         <Dropdown.Item href="/d">
-                          {t("심심풀이")}
+                          {t('심심풀이')}
                           &nbsp;&nbsp;
                           <Label
                             color="pink"
                             size="small"
-                            style={{ position: "absolute", top: "9px" }}
+                            style={{ position: 'absolute', top: '9px' }}
                           >
                             Desktop Only
                           </Label>
                         </Dropdown.Item>
                       ) : null}
                       <Dropdown.Divider />
-                      {i18n.language !== "jp" && i18n.language !== "en" ? (
+                      {i18n.language !== 'jp' && i18n.language !== 'en' ? (
                         <Dropdown.Item href="/changelog">
                           업데이트 기록
                         </Dropdown.Item>
                       ) : null}
                     </Dropdown.Menu>
                   </Dropdown>
-                  {i18n.language !== "kr" ? (
+                  {i18n.language !== 'kr' ? (
                     <Menu.Item
                       as="a"
                       style={{
-                        width: "16%",
-                        display: "inline-block",
-                        textAlign: "center",
+                        width: '16%',
+                        display: 'inline-block',
+                        textAlign: 'center',
                       }}
                       onClick={() => {
-                        i18n.changeLanguage("kr");
+                        i18n.changeLanguage('kr');
                       }}
                     >
                       KR
                     </Menu.Item>
                   ) : null}
-                  {i18n.language !== "jp" ? (
+                  {i18n.language !== 'jp' ? (
                     <Menu.Item
                       as="a"
                       style={{
-                        width: "16%",
-                        display: "inline-block",
-                        textAlign: "center",
+                        width: '16%',
+                        display: 'inline-block',
+                        textAlign: 'center',
                       }}
                       onClick={() => {
-                        i18n.changeLanguage("jp");
+                        i18n.changeLanguage('jp');
                       }}
                     >
                       JP
                     </Menu.Item>
                   ) : null}
-                  {i18n.language !== "en" ? (
+                  {i18n.language !== 'en' ? (
                     <Menu.Item
                       as="a"
                       style={{
-                        width: "16%",
-                        display: "inline-block",
-                        textAlign: "center",
+                        width: '16%',
+                        display: 'inline-block',
+                        textAlign: 'center',
                       }}
                       onClick={() => {
-                        i18n.changeLanguage("en");
+                        i18n.changeLanguage('en');
                       }}
                     >
                       EN
